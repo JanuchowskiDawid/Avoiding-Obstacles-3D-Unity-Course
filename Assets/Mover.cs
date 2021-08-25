e0b173c767edf73bf8d32f0bd39f6483aa9b67ef
mmalpa <mmalpa97@wp.pl>
2021-08-25 19:49:56 +0200

    Added player, movement script, materials, serializeFields to control velocity

---
+++ Assets/Mover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] float xVelocity = 0;
+    [SerializeField] float yVelocity = 0.01f;
+    [SerializeField] float zVelocity = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(xVelocity, yVelocity, zVelocity);
+    }
+}
