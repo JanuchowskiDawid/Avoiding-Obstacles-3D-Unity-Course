d02dd3edca1d3e6d1532f39e70b65265049e7ab1
mmalpa <mmalpa97@wp.pl>
2021-09-01 20:06:49 +0200

    Created a cube that drops after peroid of time

--- Assets/Scripts/Dropper.cs
+++ Assets/Scripts/Dropper.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Dropper : MonoBehaviour
 {
+    MeshRenderer renderer;
+    Rigidbody rigidBody;
+    [SerializeField] float dropTime = 3f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        renderer = GetComponent<MeshRenderer>();
+        rigidBody = GetComponent<Rigidbody>();
+
+        renderer.enabled = false;
+        rigidBody.useGravity = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Time.time > dropTime)
+        {
+            rigidBody.useGravity = true;
+            renderer.enabled = true;
+        }
     }
 }
